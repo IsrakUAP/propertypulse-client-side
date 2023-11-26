d885a7bece42b82e6a0acc814f52ad16c01691a6
IsrakUAP <israk2301@gmail.com>
2023-11-26 23:55:52 +0600

    AllPropertyPage is created for displaying all properties list

---
+++ src/pages/AllPropertiesPage/AllPropertiesPage.jsx
@@ -0,0 +1,31 @@
+import SectionHeader from "../../Components/SectionHeader/SectionHeader";
+import useProperties from "../../Hooks/useProperties";
+import Navbar from "../Shared/Navbar/Navbar";
+
+const AllPropertiesPage = () => {
+    const [properties] = useProperties();
+    return (
+        <div className="container mx-auto">
+            <Navbar></Navbar>
+       <SectionHeader heading={"All Properties List"}></SectionHeader>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {properties.map((property, index) => (
+            <div key={index} className="bg-white p-4 rounded-lg shadow-md">
+              <img src={property.propertyImage} alt="Property" className="w-full h-48 object-cover mb-4" />
+              <h3 className="text-xl font-semibold mb-2">{property.propertyTitle}</h3>
+              <p className="text-gray-600 mb-2">Location: {property.propertyLocation}</p>
+              <p className="text-gray-600 mb-2">Agent: {property.agentName}</p>
+              <img src={property.agentImage} alt="Agent" className="w-8 h-8 rounded-full mb-2" />
+              <p className="text-gray-600 mb-2">Verification Status: {property.verificationStatus}</p>
+              <p className="text-gray-600 mb-4">Price Range: {property.priceRange}</p>
+              <button className="bg-green-400 w-full text-white py-2 px-4 rounded-full hover:bg-gray-300">
+                Details
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+};
+
+export default AllPropertiesPage;
